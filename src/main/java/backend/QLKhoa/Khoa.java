d3d155ab7456ab6dea2bffe628a8147ecdb81fb2
BuiBao303 <hongbao2003@gmail.com>
2023-02-27 21:13:17 +0700

    test

--- src/main/java/backend/QLKhoa/Khoa.java
+++ src/main/java/backend/QLKhoa/Khoa.java
@@ -1,16 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.mycompany.qlsv;
+package backend.QLKhoa;
 
 /**
  *
  * @author ASUS
  */
-public class QLSV {
-
+public class Khoa {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hi");
     }
 }
